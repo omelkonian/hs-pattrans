a9a708fb77a7653df245df51474820516e66658b
Orestis Melkonian <melkon.or@gmail.com>
2018-09-11 15:43:46 +0200

    MIREX parser setup

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
-main :: IO ()
-main = putStrLn "MIREX"
+import Mirex (decode)
 
+main :: IO ()
+main = do
+  entries <- decode "JKUPDD-Aug2013/groundTruth/bachBWV889Fg/monophonic/csv/wtc2f20.csv"
+  print entries
